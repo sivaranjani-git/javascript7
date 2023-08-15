383b082b43ee16afd64d28a45246070d9db9e4d3
sivaranjani-git <134845313+sivaranjani-git@users.noreply.github.com>
2023-08-15 22:15:49 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,27 @@
+console.log(x);
+console.log(hello);
+console.log(y);
+
+
+var x=20;
+let y=20;
+
+//function  expression
+var hello=function hello() {
+    console.log("hello world");
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
